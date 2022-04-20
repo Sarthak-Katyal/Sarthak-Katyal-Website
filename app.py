06389c792c904a78023d388a1dc6234980bb993b
Sarthak Katyal <sarthakkatyal@wirelessprv-10-195-33-61.near.illinois.edu>
2022-04-19 19:55:52 -0500

    first commit

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("fvsldm")
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    df = pd.read_csv('2022 SP.csv')
+    return render_template('home.html', classes = df)
+
---
+++ random.py
@@ -0,0 +1,6 @@
+from gettext import install
+import pandas as pd
+
+# file = open('2022-sp.csv')
+
+pd.read_csv('2022 SP.csv')
