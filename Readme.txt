c14b01df5d0b2ab0fc64fc8b8ec52d8489284225
Sarthak Katyal <sarthakkatyal@wirelessprv-10-195-33-61.near.illinois.edu>
2022-05-19 00:55:37 -0400

    Mark II.I

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+Template Name: MyResume
+Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
+Author: BootstrapMade.com
+License: https://bootstrapmade.com/license/
+
+This is my website. Neil & Abhi you guys are goated for helping me with this. 
+But ya, simply it is a resume/portfolio website made through flask, python, and a couple templates. 
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 import pandas as pd
 from json import load
 
@@ -8,6 +8,6 @@ app = Flask(__name__)
 def foo():
     return render_template('index.html')
 
-@app.route("/static/<path:path>")
-def send_static(path):
-    return send_from_directory("static", path)
+@app.route("/assets/<path:path>")
+def send_assets(path):
+    return send_from_directory("assets", path)
